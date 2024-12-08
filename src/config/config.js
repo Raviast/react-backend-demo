ac0b3bc93babce5b8e11666c17273c5acd22a6d4
Raviast <raviastare6@gmail.com>
2024-12-08 11:17:50 +0530

    config file change

--- src/config/config.js
+++ src/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    // dbUrl:`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.ngfwe.mongodb.net/RealTrust`,
+    dbUrl:`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@cluster0.ngfwe.mongodb.net/RealTrust`,
     
-    dbUrl:'mongodb://localhost:27017/test',
+    // dbUrl:'mongodb://localhost:27017/test',
    
    
 }
